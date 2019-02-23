c0053d9595af882818a9700f376d5ad82143ebc2
Eunjae Jang <eunjaejang@Eunjaes-MacBook-Pro.local>
2019-02-24 01:14:23 +0900

    study on hierarchy

---
+++ d3_hierarchy/data/test.js
@@ -0,0 +1,172 @@
+var treeData = {
+    "name": "port",
+    "children": [{
+        "name": "HK",
+        "children": [{
+            "name": "40101010",
+            "children": [{
+                "name": "11 HK Equity",
+                "value": 2321978.63
+            }, {
+                "name": "23 HK Equity",
+                "value": 580240.45
+            }]
+        }, {
+            "name": "25301010",
+            "children": [{
+                "name": "1128 HK Equity",
+                "value": 499703.74
+            }, {
+                "name": "1928 HK Equity",
+                "value": 1575901.68
+            }, {
+                "name": "2282 HK Equity",
+                "value": 243130.03
+            }]
+        }, {
+            "name": "40301020",
+            "children": [{
+                "name": "1299 HK Equity",
+                "value": 15124876.24
+            }]
+        }, {
+            "name": "60102010",
+            "children": [{
+                "name": "101 HK Equity",
+                "value": 601509.97
+            }, {
+                "name": "12 HK Equity",
+                "value": 992113.76
+            }, {
+                "name": "16 HK Equity",
+                "value": 3543269.81
+            }, {
+                "name": "17 HK Equity",
+                "value": 1286507.77
+            }, {
+                "name": "19 HK Equity",
+                "value": 769930.62
+            }, {
+                "name": "20 HK Equity",
+                "value": 708346.87
+            }]
+        }, {
+            "name": "55101010",
+            "children": [{
+                "name": "1038 HK Equity",
+                "value": 699634.29
+            }, {
+                "name": "2 HK Equity",
+                "value": 2546646.53
+            }]
+        }, {
+            "name": "60102030",
+            "children": [{
+                "name": "1113 HK Equity",
+                "value": 2828265.07
+            }]
+        }, {
+            "name": "60102020",
+            "children": [{
+                "name": "14 HK Equity",
+                "value": 446873.98
+            }, {
+                "name": "1972 HK Equity",
+                "value": 609704.57
+            }, {
+                "name": "1997 HK Equity",
+                "value": 1118268.06
+            }]
+        }]
+    }, {
+        "name": "JP",
+        "children": [{
+            "name": "20202010",
+            "children": [{
+                "name": "2181 JT Equity",
+                "value": 321657.61
+            }]
+        }, {
+            "name": "30202030",
+            "children": [{
+                "name": "2002 JT Equity",
+                "value": 1154952.9
+            }, {
+                "name": "2212 JT Equity",
+                "value": 1074731.88
+            }, {
+                "name": "2229 JT Equity",
+                "value": 268048.01
+            }, {
+                "name": "2267 JT Equity",
+                "value": 824945.65
+            }, {
+                "name": "2269 JT Equity",
+                "value": 1028777.17
+            }, {
+                "name": "2282 JT Equity",
+                "value": 381259.06
+            }]
+        }, {
+            "name": "10102020",
+            "children": [{
+                "name": "1605 JT Equity",
+                "value": 974387.68
+            }]
+        }, {
+            "name": "20103010",
+            "children": [{
+                "name": "1801 JT Equity",
+                "value": 1019184.78
+            }, {
+                "name": "1802 JT Equity",
+                "value": 622604.17
+            }, {
+                "name": "1803 JT Equity",
+                "value": 482581.52
+            }, {
+                "name": "1812 JT Equity",
+                "value": 654839.67
+            }, {
+                "name": "1963 JT Equity",
+                "value": 307864.13
+            }]
+        }, {
+            "name": "60102010",
+            "children": [{
+                "name": "1878 JT Equity",
+                "value": 1043278.99
+            }, {
+                "name": "1925 JT Equity",
+                "value": 1820195.65
+            }]
+        }, {
+            "name": "50203010",
+            "children": [{
+                "name": "2371 JT Equity",
+                "value": 2464623.19
+            }]
+        }, {
+            "name": "25201030",
+            "children": [{
+                "name": "1928 JT Equity",
+                "value": 1028489.58
+            }]
+        }, {
+            "name": "20201080",
+            "children": [{
+                "name": "2331 JT Equity",
+                "value": 323894.93
+            }]
+        }]
+    }, {
+        "name": "DE",
+        "children": [{
+            "name": "15101050",
+            "children": [{
+                "name": "1COV GY Equity",
+                "value": 2120637.94
+            }]
+        }]
+    }]
+}
---
+++ d3_hierarchy/data/test2.js
@@ -0,0 +1,44 @@
+var treeData = {
+    "name": "Top Level",
+    "value": 10,
+    "children": [{
+            "name": "Level 2: A",
+            "value": 15,
+            "children": [{
+                    "name": "Son of A",
+                    "value": 5,
+                },
+                {
+                    "name": "Daughter of A",
+                    "value": 8,
+                }
+            ]
+        },
+        {
+            "name": "Level 2: B",
+            "value": 10,
+        }
+    ]
+}
+
+// var treeData = [
+//   {
+//     "name": "Top Level",
+//     "children": [
+//       {
+//         "name": "Level 2: A",
+//         "children": [
+//           {
+//             "name": "Son of A",
+//           },
+//           {
+//             "name": "Daughter of A",
+//           }
+//         ]
+//       },
+//       {
+//         "name": "Level 2: B",
+//       }
+//     ]
+//   }
+// ];
